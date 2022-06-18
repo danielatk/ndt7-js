4eaf1f0965b145bb4d3f12222de18b6ffc793b5c
Daniel <danatkhoo@yahoo.com.br>
2022-06-18 17:42:52 -0300

    adding possibility to send results to independent server

--- examples/node-client.js
+++ examples/node-client.js
@@ -6,10 +6,22 @@
 // reading this) should change this line to:
 // const ndt7 = require('@m-lab/ndt7');
 const ndt7 = require('../src/ndt7.js');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+const myArgs = process.argv.slice(2);
+
+if (myArgs.length != 2) {
+  throw new Error('upload server and mac must be provided via command line');
+}
+
+const uploadServer = myArgs[0];
+const mac = myArgs[1];
+let measurementServer = '';
 
 ndt7.test(
     {
       userAcceptedDataPolicy: true,
+      // server: 'mlab1-gig03.mlab-oti.measurement-lab.org',
     },
     {
       serverChosen: function(server) {
@@ -17,8 +29,20 @@ ndt7.test(
           machine: server.machine,
           locations: server.location,
         });
+        measurementServer = server.machine;
       },
       downloadComplete: function(data) {
+        const filename =
+            ''+measurementServer+'_'+mac+'_'+Date.now()+'_ndt.json';
+        const xhr = new XMLHttpRequest();
+        try {
+          xhr.open('POST', uploadServer + '/' + filename, true);
+          xhr.setRequestHeader('Content-type', 'application/json');
+          xhr.send(JSON.stringify(data));
+        } catch (e) {
+          console.error('Upload server not reachable.');
+        }
+
         // (bytes/second) * (bits/byte) / (megabits/bit) = Mbps
         const serverBw = data.LastServerMeasurement.BBRInfo.BW * 8 / 1000000;
         const clientGoodput = data.LastClientMeasurement.MeanClientMbps;
